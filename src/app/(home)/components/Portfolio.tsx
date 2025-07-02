2f490b922f7e936cf1a203b8d8bea148d8a5ea27
dung170920 <dungnguyen09172000@gmail.com>
2025-07-02 18:05:17 +0700

    fix swiper

--- src/app/(home)/components/Portfolio.tsx
+++ src/app/(home)/components/Portfolio.tsx
@@ -2,7 +2,7 @@
 import { Button } from '@/components/ui/Button'
 import Tag from '@/components/ui/Tag'
 import { ArrowLeftSmall, ArrowRightSmall, ArrowUpRight } from 'magicon'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -29,6 +29,11 @@ const portfolio = [
 const Portfolio = () => {
   const prevRef = useRef(null)
   const nextRef = useRef(null)
+  const [swiperReady, setSwiperReady] = useState(false)
+
+  useEffect(() => {
+    setSwiperReady(true)
+  }, [])
 
   return (
     <div className='lg:mb-16 mb-8 relative'>
@@ -45,41 +50,43 @@ const Portfolio = () => {
         <Button href='/' variant={"outline"} intent={'dark'}>See More <ArrowRightSmall size={20} /></Button>
       </div>
 
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        modules={[Navigation]}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-      >
-        {portfolio.map(item => (
-          <SwiperSlide key={item.id} className='flex flex-col'>
-            <div className="grid md:grid-cols-2 grid-cols-1 gap-5 items-center mb-10 mt-14 container px-6 md:px-12">
-              <div className="rounded-lg border-8 border-white overflow-hidden aspect-thumbnail shadow-[0_6px_24px_rgba(0,0,0,.08)]">
-                <Image src={item.img[0]} alt="" height={1000} width={1000} className='h-full w-full' />
+      {swiperReady && (
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[Navigation]}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+        >
+          {portfolio.map(item => (
+            <SwiperSlide key={item.id} className='flex flex-col'>
+              <div className="grid md:grid-cols-2 grid-cols-1 gap-5 items-center mb-10 mt-14 container px-6 md:px-12">
+                <div className="rounded-lg border-8 border-white overflow-hidden aspect-thumbnail shadow-[0_6px_24px_rgba(0,0,0,.08)]">
+                  <Image src={item.img[0]} alt="" height={1000} width={1000} className='h-full w-full' />
+                </div>
+                <div className="rounded-lg border-8 border-white overflow-hidden aspect-thumbnail shadow-[0_6px_24px_rgba(0,0,0,.08)]">
+                  <Image src={item.img[1]} alt="" height={1000} width={1000} className='h-full w-full' />
+                </div>
               </div>
-              <div className="rounded-lg border-8 border-white overflow-hidden aspect-thumbnail shadow-[0_6px_24px_rgba(0,0,0,.08)]">
-                <Image src={item.img[1]} alt="" height={1000} width={1000} className='h-full w-full' />
+              <div className="flex gap-4 items-center justify-center">
+                {item.types.map(type => (
+                  <span className='rounded-full md:px-3 md:py-1.5 px-2 py-1 lg:text-lg text-sm bg-grayscale-100 text-grayscale-400' key={type}>{type}</span>
+                ))}
               </div>
-            </div>
-            <div className="flex gap-4 items-center justify-center">
-              {item.types.map(type => (
-                <span className='rounded-full md:px-3 md:py-1.5 px-2 py-1 lg:text-lg text-sm bg-grayscale-100 text-grayscale-400' key={type}>{type}</span>
-              ))}
-            </div>
-            <h3 className='font-mono fs-32 text-center mt-6 mb-2'> {item.label}
-              <span className='inline-flex items-center justify-center rounded-full bg-primary size-10 ml-2'><ArrowUpRight /></span>
-            </h3>
-            <p className='leading-relaxed text-lg lg:max-w-3xl max-w-xl text-center mx-auto text-grayscale-500'>{item.description}</p>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <button className="absolute lg:left-20 left-4 top-1/2 -translate-y-1/2 md:size-14 size-10 rounded-full border border-grayscale-800 flex items-center justify-center cursor-pointer hover:bg-grayscale-800 hover:text-white z-10 bg-white" ref={prevRef}>
+              <h3 className='font-mono fs-32 text-center mt-6 mb-2'> {item.label}
+                <span className='inline-flex items-center justify-center rounded-full bg-primary size-10 ml-2'><ArrowUpRight /></span>
+              </h3>
+              <p className='leading-relaxed text-lg lg:max-w-3xl max-w-xl text-center mx-auto text-grayscale-500'>{item.description}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+      <button className="absolute lg:left-20 left-4 top-1/2 -translate-y-1/2 md:size-14 size-10 rounded-full border border-grayscale-800 flex items-center justify-center not-disabled:cursor-pointer not-disabled:hover:bg-grayscale-800 not-disabled:hover:text-white z-10 bg-white disabled:border-grayscale-400" ref={prevRef}>
         <ArrowLeftSmall size={28} />
       </button>
-      <button className="absolute lg:right-20 right-4 top-1/2 -translate-y-1/2 md:size-14 size-10 rounded-full border border-grayscale-800 flex items-center justify-center cursor-pointer hover:bg-grayscale-800 hover:text-white z-10 bg-white" ref={nextRef}>
+      <button className="absolute lg:right-20 right-4 top-1/2 -translate-y-1/2 md:size-14 size-10 rounded-full border border-grayscale-800 flex items-center justify-center not-disabled:cursor-pointer not-disabled:hover:bg-grayscale-800 not-disabled:hover:text-white z-10 bg-white disabled:border-grayscale-400" ref={nextRef}>
         <ArrowRightSmall size={28} />
       </button>
     </div>
--- src/app/(home)/components/Testimonials.tsx
+++ src/app/(home)/components/Testimonials.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Tag from '@/components/ui/Tag'
 import StarIcon from '@/assets/icons/star.svg'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -18,6 +18,11 @@ const testimonials = [
 const Testimonials = () => {
   const prevRef = useRef(null)
   const nextRef = useRef(null)
+  const [swiperReady, setSwiperReady] = useState(false)
+
+  useEffect(() => {
+    setSwiperReady(true)
+  }, [])
 
   return (
     <div className='text-white relative'>
@@ -38,45 +43,47 @@ const Testimonials = () => {
           </h2>
           <p className='text-center mx-auto max-w-3xl leading-relaxed text-grayscale-300 text-xl'>Read through our testimonials to see why our clients love working with us and how we can help you achieve your business goals through creative and effective design.</p>
         </div>
-        <Swiper
-          spaceBetween={24}
-          slidesPerView={'auto'}
-          modules={[Navigation]}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
-          initialSlide={1}
-          centeredSlides
-        >
-          {testimonials.map(item => (
-            <SwiperSlide key={item.id} className='max-w-[680px] w-full pt-12'>
-              {({ isActive }) => (
-                <div className={`flex flex-col bg-grayscale-700 rounded-lg md:px-10 px-8 md:py-6 py-4 gap-10 ${isActive ? 'opacity-100 -translate-y-4' : 'opacity-40'}`}>
-                  <div className="flex items-center justify-between">
-                    <div className="flex gap-4">
-                      <div className="md:size-16 size-12 rounded-full shrink-0">
-                        <Image src={item.avatar} alt="" width={64} height={64} className='rounded-full object-cover w-full h-full' />
+        {swiperReady && (
+          <Swiper
+            spaceBetween={24}
+            slidesPerView={'auto'}
+            modules={[Navigation]}
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            initialSlide={1}
+            centeredSlides
+          >
+            {testimonials.map(item => (
+              <SwiperSlide key={item.id} className='max-w-[680px] w-full pt-12'>
+                {({ isActive }) => (
+                  <div className={`flex flex-col bg-grayscale-700 rounded-lg md:px-10 px-8 md:py-6 py-4 gap-10 ${isActive ? 'opacity-100 -translate-y-4' : 'opacity-40'}`}>
+                    <div className="flex items-center justify-between">
+                      <div className="flex gap-4">
+                        <div className="md:size-16 size-12 rounded-full shrink-0">
+                          <Image src={item.avatar} alt="" width={64} height={64} className='rounded-full object-cover w-full h-full' />
+                        </div>
+                        <div>
+                          <h5 className='fs-24 mb-1.5'>{item.name}</h5>
+                          <span className='text-grayscale-200 text-sm md:text-base'>{item.position}</span>
+                        </div>
                       </div>
-                      <div>
-                        <h5 className='fs-24 mb-1.5'>{item.name}</h5>
-                        <span className='text-grayscale-200 text-sm md:text-base'>{item.position}</span>
-                      </div>
+                      <QuoteUpIcon />
                     </div>
-                    <QuoteUpIcon />
+                    <p>{item.content}</p>
                   </div>
-                  <p>{item.content}</p>
-                </div>
-              )}
+                )}
 
-            </SwiperSlide>
-          ))}
-        </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
         <div className="flex items-center justify-center gap-4 md:my-10 my-5">
-          <button className="md:size-12 size-10 rounded-full border border-white flex items-center justify-center cursor-pointer hover:bg-grayscale-500" ref={prevRef}>
+          <button className="md:size-12 size-10 rounded-full border border-white flex items-center justify-center not-disabled:cursor-pointer not-disabled:hover:bg-grayscale-500" ref={prevRef}>
             <ArrowLeftSmall size={28} />
           </button>
-          <button className="md:size-12 size-10 rounded-full border border-white flex items-center justify-center cursor-pointer hover:bg-grayscale-500" ref={nextRef}>
+          <button className="md:size-12 size-10 rounded-full border border-white flex items-center justify-center not-disabled:cursor-pointer not-disabled:hover:bg-grayscale-500" ref={nextRef}>
             <ArrowRightSmall size={28} />
           </button>
         </div>
